6477f5fc44aa20c4d00cab205cf7917dd3ea8f28
Bruno Diaz <brunjo12@hotmail.com>
2021-10-24 07:52:50 -0500

    servicios de usuario

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -1,6 +1,9 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Client } from 'pg';
+import { Repository } from 'typeorm';
+import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
 
 import { Order } from '../entity/order';
 import { User } from '../entity/user';
@@ -10,23 +13,35 @@ import { ProductsService } from './../../products/service/products.service';
 @Injectable()
 export class UserService {
   constructor(
+    @InjectRepository(User) private repository: Repository<User>,
     private productService: ProductsService,
     private configService: ConfigService,
     @Inject('PG') private clientPg: Client,
   ) { }
 
-  private users: User[] = [
-    {
-      id: 1,
-      firstname: 'Bruno',
-      lastname: 'Diaz',
-      email: 'bruno@hotmail',
-      password: '123456',
-    },
-  ];
+  getAllUsers() {
+    return this.repository.find();
+  }
+
+  createUser(payload: CreateUserDto) {
+    return this.repository.save(payload);
+  }
+
+  async updateUser(id: number, payload: UpdateUserDto) {
+    const user = await this.repository.findOne(id);
+    if (!user) {
+      throw new NotFoundException('El usuario ' + id + ' no existe');
+    }
+    this.repository.merge(user, payload);
+    return this.repository.save(user);
+  }
+
+  deleteUser(id: number) {
+    return this.repository.delete(id);
+  }
 
   async getOrdersByUser(id: number) {
-    const user = this.findOne(id);
+    const user = this.repository.findOne(id);
     const apiKey = this.configService.get('API_KEY');
     console.log('ApiKey ' + apiKey);
     return {
@@ -34,14 +49,6 @@ export class UserService {
       user,
       products: await this.productService.findAll(),
     };
-  }
-
-  findOne(id: number) {
-    const user = this.users.find((item) => item.id == id);
-    if (!user) {
-      throw new NotFoundException('El usuario de codigo ' + id + ' no existe');
-    }
-    return user;
   }
 
   getTasks() {
